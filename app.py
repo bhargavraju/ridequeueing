d2bd58ac874160898970cdf2165c31b82d6a07fe
Bhargav <jampanibhargavraju@gmail.com>
2020-02-11 22:18:27 +0530

    Driver select rides functionality

--- app.py
+++ app.py
@@ -63,24 +63,31 @@ def customer_app():
         return render_template('customerapp.html', title='Customer App', form=form)
 
 
-# @app.route('/customer/request', methods=['POST'])
-# def create_request():
-#     """
-#     Creates a new ride request by a customer
-#     :param: customer_id: str
-#     :return: Success Message
-#     """
-#     inputs = request.get_json(force=True)
-#     customer_id = inputs['customer_id']
-#     request_data.create_request(customer_id)
-#     return "Request for a ride created successfully", 201
+@app.route('/customer/request', methods=['POST'])
+def create_request():
+    """
+    Creates a new ride request by a customer
+    :param: customer_id: str
+    :return: Success Message
+    """
+    inputs = request.get_json(force=True)
+    customer_id = inputs['customer_id']
+    request_data.create_request(customer_id)
+    return "Request for a ride created successfully", 201
 
 @app.route("/driverapp/<driver_id>", methods=['GET', 'POST'])
 def driver_app(driver_id):
+    error = False
+    if request.method == 'POST':
+        request_id = request.form['accept']
+        if not request_data.request_available(request_id):
+            error = True
+        else:
+            request_data.accept_request(request_id, driver_id)
     waiting = request_data.get_waiting_requests()
     ongoing = request_data.get_ongoing_requests(driver_id)
     completed = request_data.get_completed_requests(driver_id)
-    return render_template('driverapp.html', title='Driver App',
+    return render_template('driverapp.html', title='Driver App', error=error,
                            driver_id=driver_id, waiting=waiting, ongoing=ongoing, completed=completed)
 
 
