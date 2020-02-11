68c269f6101f53fcd5500d25222881c7232104d7
Bhargav <jampanibhargavraju@gmail.com>
2020-02-11 18:27:11 +0530

    Db creation script

---
+++ db_script.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS app_db;
+CREATE DATABASE IF NOT EXISTS app_db;
+USE app_db;
+CREATE TABLE invoice (
+    request_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    driver_id INT DEFAULT NULL,
+    customer_id INT NOT NULL,
+    request_time DATETIME NOT NULL DEFAULT now(),
+    picked_up DATETIME DEFAULT NULL,
+    completed DATETIME DEFAULT NULL
+    );
